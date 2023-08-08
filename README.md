1a8dee03852ed07f01e702d77277cea85e11e75f
Tim Holy <tim.holy@gmail.com>
2023-08-08 05:07:11 -0500

    Rework using purely linear programming

--- README.md
+++ README.md
@@ -12,21 +12,22 @@ The optimization criteria are as follows:
 - penalize having students paired in more than one group.
 
 Here's a quick demo, setting up 6 students from 3 different programs; week 1 has just two groups, while week 2 has 3 groups.
-The student preferences are captured in the variable `interests` below.
+Students are identified by first and last name; in this example, all students have first name "Student".
+The student preferences are captured in the variable `preferences` below.
 
 ```julia
 julia> using AssignGroups
 
-julia> students = [Student("Student"*s, "Program"*p) for (s, p) in zip('A':'F', "123123")]
+julia> students = [Student("Student", s, "Program"*p) for (s, p) in zip('A':'F', "123123")]
 6-element Vector{Student}:
- Student("StudentA", "Program1", Int64[])
- Student("StudentB", "Program2", Int64[])
- Student("StudentC", "Program3", Int64[])
- Student("StudentD", "Program1", Int64[])
- Student("StudentE", "Program2", Int64[])
- Student("StudentF", "Program3", Int64[])
+ Student A (Program1)
+ Student B (Program2)
+ Student C (Program3)
+ Student D (Program1)
+ Student E (Program2)
+ Student F (Program3)
 
-julia> interests = (# Easy case (perfect solution available): two weeks, students have disjoint interests
+julia> preferences = (# Easy case (perfect solution available): two weeks, students have disjoint preferences
     [1 4;    # Week 1: student A scores group 1 as a 1 and group 2 as a 4
      1 4;    # students B&C give the same scores as A
      1 4;
@@ -34,28 +35,21 @@ julia> interests = (# Easy case (perfect solution available): two weeks, student
      4 1;
      4 1],
     [1 4 4; # Week 2: now there are 3 groups (no relation to the previous week, the groups get novel assignments)
-     4 1 4; # Students who shared interests during week 1 are non-overlapping
+     4 1 4; # Students who shared preferences during week 1 are non-overlapping
      4 4 1;
      4 1 4; # And students in the same program are also not overlapping
      4 4 1;
      1 4 4],
 );
 
-julia> assign!(students, interests)
-
-******************************************************************************
-This program contains Ipopt, a library for large-scale nonlinear optimization.
- Ipopt is released as open source code under the Eclipse Public License (EPL).
-         For more information visit https://github.com/coin-or/Ipopt
-******************************************************************************
-
+julia> assign!(students, preferences)
 6-element Vector{Student}:
- Student("StudentA", "Program1", [1, 1])
- Student("StudentB", "Program2", [1, 2])
- Student("StudentC", "Program3", [1, 3])
- Student("StudentD", "Program1", [2, 2])
- Student("StudentE", "Program2", [2, 3])
- Student("StudentF", "Program3", [2, 1])
+ Student A (Program1): [1, 1]
+ Student B (Program2): [1, 2]
+ Student C (Program3): [1, 3]
+ Student D (Program1): [2, 2]
+ Student E (Program2): [2, 3]
+ Student F (Program3): [2, 1]
 ```
 
 The output here means that:
@@ -73,12 +67,15 @@ This is the "perfect case," but you cannot count on that always being achievable
 To get an overall sense of the solution, you can use
 
 ```
-julia> printstats(students, interests)
+julia> printstats(students, preferences)
 Mean preference score: 1.0
+Maximum imbalance in group size: 0
+
 Two or more students from the same program assigned to the same group ("program collisions"):
   Total number of program collisions: 0
   Maximum number of collisions in a single group: 0
-  Number of times each program appears in a collision: Dict{String, Int64}()
+  Number of times each program appears in a collision:
+
 Two or more students sharing a group in more than one week ("student collisions"):
   Total number of student collisions: 0
   Maximum number of collisions for a single pair: 1
@@ -87,15 +84,16 @@ Two or more students sharing a group in more than one week ("student collisions"
 The `printstats` call is intended to help you tune the fitting parameters to avoid undesirable outcomes:
 
 ```julia
-assign!(students, interests; penalty_sameprogram=5, penalty_samepartner=10)
+assign!(students, preferences; penalty_sameprogram=1, penalty_samepartner=1, penalty_sizeimbalance=1)
 ```
 
 Increase:
 
 - `penalty_sameprogram` if you get too many "program collisions"
 - `penalty_samepartner` if you get too many "student collisions"
+- `penalty_sizeimbalance` if the maximum imbalance in group size is bigger than you want
 
 Note that these can be in conflict with each other, you may not be able to avoid all collisions.
-Likewise, the more you penalize collisions, the less well you respect student `interests`.
+Likewise, the more you penalize collisions, the less well you respect student `preferences`.
 
 Finally, you can manually perform early assignments and compute the remainder; if `student.assigned` is not empty upon calling `assign!`, the existing choices are retained. Use `unassign!(students)` to wipe out all existing assignments.
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 @testset "AssignGroups" begin
     students = [Student("Student"*s, "Last", "Program"*p) for (s, p) in zip('A':'F', "123123")]
-    # Easy case (perfect solution available): one week, students from same program have disjoint interests
-    interests = (
+    # Easy case (perfect solution available): one week, students from same program have disjoint preferences
+    preferences = (
         [1 4;
          1 4;
          1 4;
@@ -12,11 +12,11 @@ using Test
          4 1;
          4 1],
     )
-    assign!(students, interests)
+    assign!(students, preferences)
     for (i, s) in enumerate(students)
         @test only(s.assigned) == 1 + (i > 3)
     end
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == 1
     @test npairs[("Last, StudentA", "Last, StudentB")] == 1
     @test npairs[("Last, StudentA", "Last, StudentC")] == 1
@@ -26,13 +26,13 @@ using Test
     @test npairs[("Last, StudentE", "Last, StudentF")] == 1
     @test get(npairs, ("Last, StudentA", "Last, StudentD"), 0) == 0
     @test isempty(nprog)
-    str = sprint(printstats, students, interests)
+    str = sprint(printstats, students, preferences)
     @test occursin(r"Mean preference score: 1.0", str)
     @test occursin(r"Two or more students from the same program assigned to the same group", str)
     @test occursin(r"Total number of program collisions: 0", str)
     @test occursin(r"Total number of student collisions: 0", str)
-    # Easy case (perfect solution available): two weeks, students from same program have disjoint interests
-    interests = (
+    # Easy case (perfect solution available): two weeks, students from same program have disjoint preferences
+    preferences = (
         [1 4;
          1 4;
          1 4;
@@ -47,11 +47,11 @@ using Test
          1 4 4],
     )
     unassign!(students)
-    assign!(students, interests)
+    assign!(students, preferences)
     for (i, s) in enumerate(students)
         @test s.assigned == [1 + (i > 3), i < 4 ? i : mod1(i+1, 3)]
     end
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == 1
     @test npairs[("Last, StudentA", "Last, StudentB")] == 1
     @test npairs[("Last, StudentA", "Last, StudentC")] == 1
@@ -69,13 +69,13 @@ using Test
     for student in students
         pop!(student.assigned)
     end
-    interests_fake = (zeros(Int, 6, 2), interests[2])
-    assign!(students, interests_fake)
+    preferences_fake = (zeros(Int, 6, 2), preferences[2])
+    assign!(students, preferences_fake)
     @test students == students0
     # Do we get a warning if all students are pre-assigned?
-    @test_logs (:warn, "All students are already assigned to groups (use `unassign!` to reset)") assign!(students, interests)
+    @test_logs (:warn, "All students are already assigned to groups (use `unassign!` to reset)") assign!(students, preferences)
     # Case with conflict: three groups, one week, students from the same program have the same top choice but differ in their second choice
-    interests = (
+    preferences = (
         [1 3 4;
          4 1 3;
          3 4 1;
@@ -84,26 +84,26 @@ using Test
          4 2 1],
     )
     unassign!(students)
-    assign!(students, interests)
+    assign!(students, preferences)
     for (i, s) in enumerate(students)
         @test only(s.assigned) == (i < 4 ? i : mod1(i-1, 3))
     end
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == 1.5
     @test isempty(nprog)
     # If we eliminate the same-program penalty, each student gets the top choice
     unassign!(students)
-    assign!(students, interests; penalty_sameprogram=0)
+    assign!(students, preferences; penalty_sameprogram=0)
     for (i, s) in enumerate(students)
         @test only(s.assigned) == (i < 4 ? i : mod1(i, 3))
     end
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == 1
     @test nprog[(1, 1, "Program1")] == 1  # during week 1, group 1 has two students from Program1
     @test nprog[(1, 2, "Program2")] == 1  # during week 1, group 2 has two students from Program2
     @test nprog[(1, 3, "Program3")] == 1  # during week 1, group 3 has two students from Program3
     # Case with conflict: two weeks, students from different programs have the same top choice but differ in their second choice
-    interests = (
+    preferences = (
         [1 4;
          1 4;
          1 4;
@@ -118,11 +118,11 @@ using Test
          1 4 3],
     )
     unassign!(students)
-    assign!(students, interests; penalty_sameprogram=0)
+    assign!(students, preferences; penalty_sameprogram=0)
     for (i, s) in enumerate(students)
         @test s.assigned == [1 + (i > 3), mod1(i, 3)]
     end
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == 1.5
     @test nprog[(2, 1, "Program1")] == 1  # during week 2, group 1 has two students from Program1
     @test nprog[(2, 2, "Program2")] == 1
@@ -130,8 +130,8 @@ using Test
     @test maximum(values(npairs)) == 1
 
     unassign!(students)
-    assign!(students, interests; penalty_samepartner=0)
-    pref, npairs, nprog = AssignGroups.analyze(students, interests)
+    assign!(students, preferences; penalty_samepartner=0)
+    pref, maximbalance, npairs, nprog = AssignGroups.analyze(students, preferences)
     @test pref == (6 + (3 + 1 + 1 + 1 + 2 + 3)) / 12
     @test isempty(nprog)
     @test any(==(2), values(npairs))
