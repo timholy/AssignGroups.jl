dd030b0f1a38f4e1c7fd0f9fb34b579b86229d58
Tim Holy <tim.holy@gmail.com>
2023-11-29 05:11:59 -0600

    Expand testing

--- test/partners.jl
+++ test/partners.jl
@@ -7,12 +7,16 @@ using CSV
 
 @testset "Partners" begin
     students = [Student("Student"*s, "Last", p) for (s, p) in zip('A':'F', [1,2,3,1,2,3])]
-    groups, status = assign(students, 2, zeros(6, 6))
+    groups, status = assign(students, 2, zeros(6, 6); attributes=("mip_rel_gap" => 1e-3,))
     @test status == OPTIMAL
     for g in groups
         @test length(g) == 3
         @test mean([s.score for s in g]) ≈ 2
     end
+    redirect_stdout(devnull) do
+        @test_logs (:error, r"TIME_LIMIT") (:info, r"Consider") assign(students, 2, zeros(6, 6); time_limit=1e-6)
+    end
+
     groups, status = assign(students, 3, zeros(6, 6))
     for g in groups
         @test length(g) == 2
